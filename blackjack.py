543af1b8fc9251606759ac1d8c03c13db197018e
Jon Gamble <jgamble3@gmail.com>
2017-03-28 21:07:26 -0400

    Refactoring to include smaller classes - built card & hand classes

--- blackjack.py
+++ blackjack.py
@@ -19,6 +19,73 @@
 # -----------------------------------------------------------------------------
 from random import shuffle
 
+class Card():
+    '''
+        Creates a card with a value and a suit
+        Value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        Suit = ['C', 'D', 'H', 'C']
+    '''
+
+    # define class variables
+    value
+    suit
+
+    # create the card
+    def __init__(self, card_value, card_suit):
+        self.value = card_value
+        self.suit = card_suit
+
+    # return the value of the card as an integer
+    def get_value(self):
+        if self.value == 'A':
+            return 11
+        elif self.value in ('K', 'Q', 'J'):
+            return 10
+        elif
+            return int(self.value)
+
+    # print the card
+    def print_card:
+        return (self.value, self.suit)
+
+class Hand():
+    '''
+        Creates a hand, which is a list of cards having a value, which is
+        summation of all card values
+    '''
+
+    # define class variables
+    hand
+    hand_total
+
+    # Create an empty hand
+    def __init__(self):
+        self.hand = []
+
+    # returns the hand total
+    def get_hand_total(self):
+
+        # initialize varaibles
+        aces_count = 0
+
+        # loop through the cards in the hand to determine hand total
+        for card in self.hand:
+
+            # increase the hand total
+            total += card.get_value()
+
+            # count for the number of aces found
+            if card.get_value() == 11:
+                aces_count += 1
+
+        while total > 21 and aces_count > 1:
+            total -= 11
+            aces_count-=1
+
+        return total
+
+class 
+
 class Blackjack:
     '''
         Creates a blackjack game, managing the creation of a deck, dealing of cards
@@ -29,6 +96,7 @@ class Blackjack:
     __dealer_total = 0
     __player_hand = []
     __player_total = 0
+    __has_winner = False
 
     def __init__(self):
 
@@ -42,6 +110,8 @@ class Blackjack:
         self.__dealer_total = self.calculate_total(self.__dealer_hand)
         self.__player_total = self.calculate_total(self.__player_hand)
 
+        # set hand_status to be True, meaning a hand is in progress
+        self.__hand_status = True
 
     def create_deck(self):
         '''
@@ -97,20 +167,42 @@ class Blackjack:
 
         return total
 
+    def has_winner(self):
+        return self.__has_winner
 
-#    def hit(self):
-
-#    def stand(self):
+    def status(self):
+        return self.__status
 
 #    def print_hands(self):
-
+        #if player_total == 21
 
 # ----------------------------------------------------------------------------
 # Main program
 #
 # ----------------------------------------------------------------------------
 
+def get_play_again():
+    while True:
+        play_again = input('would you like to play again (y/n)')
+
+        if play_again == 'y':
+            return True
+        elif play_again == 'n':
+            return False
+        else:
+            print('sorry, I did not understand your response')
+
 blackjack = Blackjack()
+play = True
+
+print('--------------------')
+print('Welcome To Blackjack')
+print('--------------------\n')
+
+while play:
+
+    play = get_play_again() #input('Would you like to play again (y/n)?')
+
 
 
 
